8ac71d71b8e20d624c2cccb96cca31cd802d9be6
JDazogbo <jdazo054@uottawa.ca>
2023-07-24 16:44:33 -0400

    Added a origin point to viewer

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -3,18 +3,16 @@ import numpy as np
 import pandas as pd
 import time
 
-data = pd.read_csv("Data\LeddarVu8-Data-0.05mins-5.0Hz.csv")
+data = pd.read_csv("Data\LeddarVu8-Data-1.0mins-5.0Hz.csv").fillna(0)
 
 pcd = o3d.geometry.PointCloud()
 
 rate = 5
 
-row = data.iloc[14]
-
 def getSetPoints(data):
-    ptsx = [data["X Coord 7"],data["X Coord 6"],data["X Coord 5"],data["X Coord 4"],data["X Coord 3"],data["X Coord 2"],data["X Coord 1"],data["X Coord 0"]]
+    ptsx = [data["X Coord 7"],data["X Coord 6"],data["X Coord 5"],data["X Coord 4"],data["X Coord 3"],data["X Coord 2"],data["X Coord 1"],data["X Coord 0"]] 
     ptsy = [data["Y Coord 7"],data["Y Coord 6"],data["Y Coord 5"],data["Y Coord 4"],data["Y Coord 3"],data["Y Coord 2"],data["Y Coord 1"],data["Y Coord 0"]]
-    return (ptsx, ptsy, [0 for i in range(8)])
+    return (ptsx , ptsy, [0 for i in range(8)])
 
 
 vis = o3d.visualization.Visualizer()
@@ -29,9 +27,9 @@ while increment<data.shape[0]:
     while not (time.time()-previous > 1/float((rate))):
         pass
     points = np.vstack(getSetPoints(data.iloc[increment])).transpose()
-    pcd.points = o3d.utility.Vector3dVector(points)
+    origin = np.array([0,0,0])
+    pcd.points = o3d.utility.Vector3dVector(np.vstack((points, origin)))
     vis.add_geometry(pcd)
-    vis.add_geometry(o3d.utility.Vector3dVector(np.vstack(getSetPoints((0,0,0))).transpose()))
     ctr.convert_from_pinhole_camera_parameters(parameters)
     vis.poll_events()
     vis.update_renderer()
